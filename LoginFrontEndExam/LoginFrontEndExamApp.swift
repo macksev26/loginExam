49af0e153df326c8a38b838aa6722d816935f9c1
mackysev <maximo@entegy.com.au>
2024-07-12 16:16:52 +0800

    Initial Commit

---
+++ LoginFrontEndExam/LoginFrontEndExamApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginFrontEndExamApp.swift
+//  LoginFrontEndExam
+//
+//  Created by Macky Sevidal on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginFrontEndExamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
